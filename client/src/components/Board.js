60f5f6d0fe4ff3f0a53a451b4b195a9668767b9c
abhishek <abhishekkumartbbt@gmail.com>
2022-07-03 09:59:39 +0530

    completed task do not show done property

--- client/src/components/Board.js
+++ client/src/components/Board.js
@@ -107,7 +107,7 @@ export default function Board() {
                     return (
                         <div key={idx+1}>
                             <div className={"edits "+ (todo.pending?'pending-task':'completed-task')}><span className="left"><span className="sno">{idx+1}</span>
-                            <span className="taskcontent">{todo.task}</span></span><span className="controls"><span onClick={()=>{doneTodo(todo._id)}} className="done"><i className="fa-solid fa-circle-check icon doneicon"></i>Done</span>
+                            <span className="taskcontent">{todo.task}</span></span><span className="controls"><span style={{display: todo.pending===1?"inline-block":"none"}} onClick={()=>{doneTodo(todo._id)}} className="done"><i className="fa-solid fa-circle-check icon doneicon"></i>Done</span>
                             <span onClick={()=>{setPrevTask(todo.task); setEditing(todo._id); window.scrollTo(0,0);}} className="edit"><i className="fa-solid fa-pen-to-square icon editicon"></i>Edit</span><span onClick={()=>{deleteTodo(todo._id)}} className="delete"><i className="fa-solid fa-trash-can icon deleteicon"></i>Delete</span></span></div>
                         </div>
                     );

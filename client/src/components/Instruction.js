b164a2adbf97d34e9523ae65cd3c942026238fd9
abhishek <abhishekkumartbbt@gmail.com>
2022-07-07 21:45:12 +0530

    styling

--- client/src/components/Instruction.js
+++ client/src/components/Instruction.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Instruction() {
   return (
     <div>
-        <ul class="points">
+        <ul className="points">
             <li>Delete all is disabled for now.</li>
             <li>You cannot play with last 15 notes. Because they are mine:)</li>
             <li>Pending has blue color.</li>
